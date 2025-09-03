4b19b1f74ca86c42b4e1606fb58ba24d9ea2e887
serkeb <sebastiangonzalezzr@gmail.com>
2025-09-03 11:32:41 -0300

    Update AppContext.jsx

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer, useEffect } from 'react'
-import { db } from '../lib/supabaseClient'
+import { db } from '../lib/supabase'
 import { toast } from 'sonner'
 import { useAuth } from './AuthContext'
 
